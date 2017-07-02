21b37db598397df9f3da78b5ebaabb887086bfc4
ppreziotti <patpreziotti@gmail.com>
2017-07-02 11:22:11 -0400

    added delete route

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -5,18 +5,25 @@ $(document).ready(function() {
   });
 
   function saveArticle() {
-    event.preventDefault();
-
     var title = $(this).data("title");
-
 	$.ajax({
       method: "PUT",
-	  url: "/api/articles/" + title,
-	  success: function() {
-	    alert("Article saved successfully!");
-	  }
+	  url: "/api/articles/" + title
+	}).done(function() {
+	  location.reload();
 	});
   }
 
+  function deleteArticle() {
+    var title = $(this).data("title");
+    $.ajax({
+      method: "DELETE",
+      url: "/api/articles/" + title
+    }).done(function() {
+      location.reload();
+    });
+  }
+
   $(document).on("click", ".btn-save", saveArticle);
+  $(document).on("click", ".btn-delete", deleteArticle);
 });
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,20 +4,47 @@ var Comment = require("../models/Comment.js");
 
 module.exports = function(app) {
   app.get("/api/articles", function(req, res) {
-    Article.find({}, function(articles) {
-      res.send(articles);
+    Article.find({}, function(error, articles) {
+      if (error) {
+      	res.send(error);
+      }
+      else {
+        res.send(articles);
+      }
     });
   });
 
+  app.get("/api/articles/:title", function(req, res) {
+  	Article.find({title: req.params.title}, function(error, article) {
+  	  if (error) {
+  	  	res.send(error);
+  	  }
+  	  else {
+  	  	res.send(article)
+  	  }
+  	})
+  })
+
   // Put route for updating saved status of article -- need to complete
   app.put("/api/articles/:title", function(req, res) {
-  	Article.findOneAndUpdate({title: req.params.title}, {$set: {saved: true}}, function(error, doc) {
+  	Article.findOneAndUpdate({title: req.params.title}, {$set: {saved: true}}, function(error) {
 	  if (error) {
-	  	console.log(error);
+	  	res.send(error);
 	  }
 	  else {
-	  	console.log(doc);
+	  	res.send("Article saved successfully!");
 	  }	
+  	});
+  });
+
+  app.delete("/api/articles/:title", function(req, res) {
+  	Article.remove({title: req.params.title}, function(error) {
+  	  if (error) {
+  	  	res.send(error);
+  	  }
+  	  else {
+  	  	res.send("Article deleted!");
+  	  }
   	});
   });
 };
